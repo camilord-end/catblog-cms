bb231368bfe55147b2a11fb3cf5eddc4375795cb
camilord-end <juliancaor10@gmail.com>
2022-06-06 15:44:14 -0500

    added basic components

---
+++ components/PostCard.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const PostCard = ({ post }) => {
+  return (
+    <div>
+      {post.title}
+      {" "}
+      {post.exerpt}
+    </div>
+  )
+}
+
+export default PostCard
