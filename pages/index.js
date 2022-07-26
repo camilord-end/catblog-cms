f10a515d3e45578bbcff377446f689bc48d6b87f
camilord-end <juliancaor10@gmail.com>
2022-07-26 14:38:53 -0500

    deleted hardcoded post to use getPost service

--- pages/index.js
+++ pages/index.js
@@ -1,20 +1,10 @@
 import Head from 'next/head'
 import { PostCard, Categories, PostWidget } from '../components'
+import { getPosts } from '../services'
 //import Image from 'next/image'
 //import styles from '../styles/Home.module.css'
 
-const posts = [
-  {
-    title: 'Cat Testing',
-    exerpt: 'Learn how to love cats',
-  },
-  {
-    title: 'Cat Testing #2',
-    exerpt: 'Learn how to love cats again',
-  },
-]
-
-export default function Home() {
+export default function Home({ posts }) {
   return (
     <div className='container mx-auto px-10 mb-8'>
       <Head>
@@ -24,7 +14,7 @@ export default function Home() {
       </Head>
       <div className='grid grid-cols-1 lg:grid-cols-12 gap-12'>
         <div className='lg:col-span-8 col-span-1'>
-          {posts.map((post) => <PostCard key={post.title} post={post} />)}
+          {posts.map((post) => <PostCard key={post.node.id} post={post.node} />)}
         </div>
         <div className='lg:col-span-4 col-span-1'>
           <div className='lg:sticky relative top-8'>
@@ -35,4 +25,13 @@ export default function Home() {
       </div>
     </div>
   )
+}
+
+export async function getStaticProps() {
+  const posts = (await getPosts()) || []
+  return {
+    props: {
+      posts,
+    },
+  }
 }
