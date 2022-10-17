a81b9209b5ef346693ed52726ccbca4c8ec50211
Julian Orozco <juliancaor10@gmail.com>
2022-10-17 10:41:03 -0500

    Added getRecentPosts and getSimilarPosts services

--- services/index.js
+++ services/index.js
@@ -1,4 +1,4 @@
-import { request, gql } from 'graphql-request';
+import { request, gql } from "graphql-request";
 
 const graphqlAPI = process.env.NEXT_PUBLIC_GRAPHCMS_ENDPOINT;
 
@@ -31,7 +31,47 @@ export const getPosts = async () => {
         }
       }
     }
-  `
+  `;
   const result = await request(graphqlAPI, query);
   return result.postsConnection.edges;
+};
+
+export const getRecentPosts = async () => {
+  const query = gql`
+    query GetPostDetails(){
+      posts(
+        orderBy: createdAt_ASC
+        last: 3
+      ) {
+        title
+        featuredImage{
+          url
+        }
+        createdAt
+        slug
+      }
+    }
+  `;
+  const result = await request(graphqlAPI, query);
+  return result.posts;
+};
+
+export const getSimilarPosts = async () => {
+  const query = gql`
+    query GetPostDetails($slug: String!, $categories: [String!]){
+      posts(
+        where: {slug_not: $slug, AND: {categories_some:{slug_in: $categories}}}
+        last: 3
+      ) {
+        title
+        featuredImage{
+          url
+        }
+        createdAt
+        slug
+      }
+    }
+  `
+    const result = await request(graphqlAPI, query);
+    return result.posts;
 }
