b7e1df78e0f42b027272f02b796379a4b42ae2fe
Julian Orozco <juliancaor10@gmail.com>
2022-10-19 13:45:20 -0500

    changed categories implementation to be dinamic

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,36 +1,38 @@
-import React, { useContext } from 'react'
-import Link from 'next/link'
-
-const categories = [
-  { name: 'Sleepy', slug: 'sleepy' },
-  { name: 'Funny', slug: 'funny' },
-]
+import React, { useEffect, useState, useContext } from 'react';
+import { getCategories } from '../services';
+import Link from 'next/link';
 
 const Header = () => {
+  const [categories, setCategories] = useState([]);
+
+  useEffect(() => {
+    getCategories().then((newCategories) => setCategories(newCategories));
+  }, []);
+
   return (
-    <div className="container mx-auto px-10 mb-8">
-      <div className="border-b w-full inline-block border-blue-400 py-8">
-        <div className="md:float-left block">
-          <Link href="/">
-            <span className="cursor-pointer font-bold text-4xl text-white">
+    <div className='container mx-auto px-10 mb-8'>
+      <div className='border-b w-full inline-block border-blue-400 py-8'>
+        <div className='md:float-left block'>
+          <Link href='/'>
+            <span className='cursor-pointer font-bold text-4xl text-white'>
               Cat CMS
             </span>
           </Link>
         </div>
-        <div className="hidden md:float-left md:contents">
+        <div className='hidden md:float-left md:contents'>
           {categories.map(({ name, slug }) => {
             return (
               <Link key={slug} href={`/category/${slug}`}>
-                <span className="md:float-right mt-2 align-middle text-white ml-4 font-semibold cursor-pointer">
+                <span className='md:float-right mt-2 align-middle text-white ml-4 font-semibold cursor-pointer'>
                   {name}
                 </span>
               </Link>
-            )
+            );
           })}
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
