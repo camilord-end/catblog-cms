f9204666a4b32ec517fa5b92ffba75f9a19d4542
Julian Orozco <juliancaor10@gmail.com>
2022-10-21 18:36:59 -0500

    Formated

--- components/PostWidget.jsx
+++ components/PostWidget.jsx
@@ -1,32 +1,32 @@
 import React, { useState, useEffect } from 'react';
 import moment from 'moment';
 import Link from 'next/link';
-import Image from 'next/image'
+import Image from 'next/image';
 
-import { getRecentPosts,getSimilarPosts } from '../services/index'
+import { getRecentPosts, getSimilarPosts } from '../services/index';
 
 const PostWidget = ({ categories, slug }) => {
   const [relatedPosts, setRelatedPosts] = useState([]);
 
   useEffect(() => {
-    if(slug){
-      getSimilarPosts(categories,slug)
-        .then((result)=>setRelatedPosts(result))
+    if (slug) {
+      getSimilarPosts(categories, slug).then((result) =>
+        setRelatedPosts(result)
+      );
     } else {
-      getRecentPosts()
-        .then((result)=>setRelatedPosts(result))
+      getRecentPosts().then((result) => setRelatedPosts(result));
     }
-  }, [slug,categories])
+  }, [slug, categories]);
 
   return (
     <div className='bg-white shadow-lg rounded-lg p-8 mb-8'>
       <h3 className='text-xl mb-8 font-semibold border-b pb-4'>
-        {slug ? 'Related Posts': 'Recent Posts'}
+        {slug ? 'Related Posts' : 'Recent Posts'}
       </h3>
-      {relatedPosts.map((post)=>(
+      {relatedPosts.map((post) => (
         <div key={post.title} className='flex items-center w-full mb-4'>
           <div className='w-16 flex-none'>
-            <Image 
+            <Image
               alt={post.title}
               height='60px'
               width='60px'
@@ -38,14 +38,18 @@ const PostWidget = ({ categories, slug }) => {
             <p className='text-gray-500 text-xs'>
               {moment(post.createdAt).format('MMM DD, YYYY')}
             </p>
-            <Link href={`/post/${post.slug}`} key={post.title} className='text-sm'>
+            <Link
+              href={`/post/${post.slug}`}
+              key={post.title}
+              className='text-sm'
+            >
               {post.title}
             </Link>
           </div>
         </div>
       ))}
     </div>
-  )
-}
+  );
+};
 
 export default PostWidget;
