f02e65421c4566cb0e4591d55bb36ff04826ab45
Julian Orozco <juliancaor10@gmail.com>
2022-10-24 13:41:50 -0500

    Added getCategoryPost / getFeaturedPosts services

--- services/index.js
+++ services/index.js
@@ -124,6 +124,41 @@ export const getCategories = async () => {
   return result.categories;
 };
 
+export const getCategoryPost = async (slug) => {
+  const query = gql`
+    query GetCategoryPost($slug: String!) {
+      postsConnection(where: { categories_some: { slug: $slug } }) {
+        edges {
+          cursor
+          node {
+            author {
+              bio
+              name
+              id
+              photo {
+                url
+              }
+            }
+            createdAt
+            slug
+            title
+            excerpt
+            featuredImage {
+              url
+            }
+            categories {
+              name
+              slug
+            }
+          }
+        }
+      }
+    }
+  `;
+  const result = await request(graphqlAPI, query, { slug });
+  return result.postsConnection.edges;
+};
+
 export const submitComment = async (obj) => {
   const result = await fetch('/api/comments', {
     method: 'POST',
@@ -147,4 +182,29 @@ export const getComments = async (slug) => {
   `;
   const result = await request(graphqlAPI, query, { slug });
   return result.comments;
+};
+
+export const getFeaturedPosts = async () => {
+  const query = gql`
+    query GetCategoryPost() {
+      posts(where: {featuredPost: true}) {
+        author {
+          name
+          photo {
+            url
+          }
+        }
+        featuredImage {
+          url
+        }
+        title
+        slug
+        createdAt
+      }
+    }   
+  `;
+
+  const result = await request(graphqlAPI, query);
+
+  return result.posts;
 };
